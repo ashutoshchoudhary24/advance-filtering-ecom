b110931716bd09f5ac2e751e436174003779a110
ashutoshchoudhary24 <ashumm.1807@gmail.com>
2024-02-02 00:00:44 +0530

    updated

---
+++ src/Products/Product.js
@@ -0,0 +1,194 @@
+import React from 'react'
+import "./Product.css"
+import { AiFillStar } from 'react-icons/ai'
+import { IoBagHandleSharp } from "react-icons/io5";
+
+function Product() {
+  return (
+    <>
+    <section className="card-container">
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+      <section className="card">
+        <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="shoe-image" />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+            <AiFillStar className='ratings-start'/>
+
+            <span className="total-reviews">4</span>
+          </section>
+     <section className="card-price">
+      <div className="price">
+        <del>$300</del> 200
+      </div>
+
+  <div className="bag">
+  <IoBagHandleSharp className='bag-icon' />
+  </div>
+
+      </section>     
+        </div>
+
+      </section>
+    </section>
+
+    
+
+    
+    </>
+  )
+}
+
+export default Product
---
+++ src/Sidebar/Color/Color.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Color.css'
+function Color() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Color
